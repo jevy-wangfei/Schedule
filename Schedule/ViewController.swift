4b4366dfdc0391b14d707dbc51972dc58cb71d03
Fei Wang <u5575182@anu.edu.au>
2015-07-01 10:30:12 +1000

    Initial Commit

---
+++ Schedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schedule
+//
+//  Created by jevy on 1/07/2015.
+//  Copyright (c) 2015 jevy.wf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
